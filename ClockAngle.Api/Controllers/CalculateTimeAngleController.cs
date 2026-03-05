f7d29f569e2132b9b916d42615e3ff23689506c1
Dominic Torruellas <dom@domtorr.com>
2026-03-05 09:01:10 -0700

    Create initial CalculateTimeAngleController

---
+++ ClockAngle.Api/Controllers/CalculateTimeAngleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClockAngle.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CalculateTimeAngleController() : ControllerBase
+{
+    // Add controller logic here
+    public IActionResult Get(
+        [FromQuery] string? time, 
+        [FromQuery] int? hour, 
+        [FromQuery] int? minute)
+    {
+        return Ok(new { time, hour, minute });
+    }
+}
